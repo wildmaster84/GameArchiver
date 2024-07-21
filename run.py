ce947bf768f73c8969bd27c771ba925641ef9aa5
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2024-07-20 23:40:34 -0400

    enabled multithreading

--- run.py
+++ run.py
@@ -2,9 +2,15 @@ from app import app
 from app.utils import gamePages
 from app.utils import rebuild_index
 import os
+from concurrent.futures import ThreadPoolExecutor
+
+def rebuild_all_indexes():
+    HOME_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Pages'))
+    game_dirs = list(gamePages(HOME_DIRECTORY))
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(rebuild_index, game_dirs)
 
 if __name__ == '__main__':
-    HOME_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Pages'))
-    for game in gamePages(HOME_DIRECTORY):
-        rebuild_index(game)
-    app.run(host='0.0.0.0', port=10038)
+    rebuild_all_indexes()
+    app.run(host='0.0.0.0', port=10038, threaded=True)
